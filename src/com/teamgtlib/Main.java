a6ff6a613b03e10e3503403f60d4dde516cd5167
bc2ma1 <bc2ma1@inf.elte.hu>
2021-02-17 13:28:54 +0100

    Make it "alma3"

--- src/com/teamgtlib/Main.java
+++ src/com/teamgtlib/Main.java
@@ -3,7 +3,7 @@ package com.teamgtlib;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("alma2");
+        System.out.println("alma3");
         // write your code here
     }
 }
