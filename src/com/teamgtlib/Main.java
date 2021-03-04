733466aba43e66006b7ac8d61114bf34bb980f1f
Kovács <dgmvhc@inf.elte.hu>
2021-03-04 13:57:07 +0100

    -

--- src/com/teamgtlib/Main.java
+++ src/com/teamgtlib/Main.java
@@ -3,10 +3,6 @@ package com.teamgtlib;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("alma3");
-        Test gyumi = new Test();
-        System.out.println(gyumi.toString());
-        System.out.println("hello");
 
         // write your code here
     }
