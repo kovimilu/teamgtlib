0d57e54a9690390c26e1f518f4a06a362b8b87fe
Kovács <dgmvhc@inf.elte.hu>
2021-03-09 14:33:52 +0100

    MAXCAP

--- src/com/teamgtlib/Buildings/Ride.java
+++ src/com/teamgtlib/Buildings/Ride.java
@@ -11,21 +11,25 @@ import java.util.ArrayList;
 public class Ride extends Building {
     Boolean BUILT = false;
     int durability;
-    int MAXCAP = 20;
+    private int MAXCAP;
     int currentCap;
     Enum type;
     ArrayList<Visitor> queue;
 
+    /*
     public Ride(int x, int y) {
         this.x = x;
         this.y = y;
+        MAXCAP = 20;
         //TODO
     }
+    */
 
     public Ride(int x, int y, Enum type) {
         this.x = x;
         this.y = y;
         this.type = type;
+        MAXCAP = 30;
         //TODO
     }
 
