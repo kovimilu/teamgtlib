52b30af2ea124de7c431ade03bd7dd04c68d6727
Kovács <dgmvhc@inf.elte.hu>
2021-03-10 19:28:40 +0100

    Decor & Ride Consts set

--- src/com/teamgtlib/Buildings/DecorConstants.java
+++ src/com/teamgtlib/Buildings/DecorConstants.java
@@ -4,18 +4,18 @@ public class DecorConstants {
     private DecorConstants() {}
 
     // TODO: fill with values
-    public static final int BENCH_PRICE     = 0;
-    public static final int BENCH_MOODVALUE = 0;
-    public static final int BENCH_WIDTH     = 0;
-    public static final int BENCH_HEIGHT    = 0;
+    public static final int BENCH_PRICE     = 5000;
+    public static final int BENCH_MOODVALUE = 10;
+    public static final int BENCH_WIDTH     = 2;
+    public static final int BENCH_HEIGHT    = 1;
 
-    public static final int BUSH_PRICE     = 0;
-    public static final int BUSH_MOODVALUE = 0;
-    public static final int BUSH_WIDTH     = 0;
-    public static final int BUSH_HEIGHT    = 0;
+    public static final int BUSH_PRICE     = 1500;
+    public static final int BUSH_MOODVALUE = 2;
+    public static final int BUSH_WIDTH     = 1;
+    public static final int BUSH_HEIGHT    = 1;
 
-    public static final int TREE_PRICE     = 0;
-    public static final int TREE_MOODVALUE = 0;
-    public static final int TREE_WIDTH     = 0;
-    public static final int TREE_HEIGHT    = 0;
+    public static final int TREE_PRICE     = 3550;
+    public static final int TREE_MOODVALUE = 5;
+    public static final int TREE_WIDTH     = 1;
+    public static final int TREE_HEIGHT    = 1;
 }
--- src/com/teamgtlib/Buildings/RideConstants.java
+++ src/com/teamgtlib/Buildings/RideConstants.java
@@ -4,33 +4,33 @@ public class RideConstants {
     private RideConstants() {}
 
     // TODO: fill with values
-    public static final int CAROUSEL_MAXCAP         = 50;
-    public static final int CAROUSEL_PRICE          = 70000;
-    public static final int CAROUSEL_MOODVALUE      = 30;
+    public static final int CAROUSEL_MAXCAP         = 30;
+    public static final int CAROUSEL_PRICE          = 55000;
+    public static final int CAROUSEL_MOODVALUE      = 25;
     public static final int CAROUSEL_WIDTH          = 3;
     public static final int CAROUSEL_HEIGHT         = 3;
 
-    public static final int DODGEM_MAXCAP           = 0;
-    public static final int DODGEM_PRICE            = 0;
-    public static final int DODGEM_MOODVALUE        = 0;
-    public static final int DODGEM_WIDTH            = 0;
-    public static final int DODGEM_HEIGHT           = 0;
+    public static final int DODGEM_MAXCAP           = 20;
+    public static final int DODGEM_PRICE            = 90000;
+    public static final int DODGEM_MOODVALUE        = 40;
+    public static final int DODGEM_WIDTH            = 4;
+    public static final int DODGEM_HEIGHT           = 3;
 
-    public static final int FERRISWHEEL_MAXCAP      = 0;
-    public static final int FERRISWHEEL_PRICE       = 0;
-    public static final int FERRISWHEEL_MOODVALUE   = 0;
-    public static final int FERRISWHEEL_WIDTH       = 0;
-    public static final int FERRISWHEEL_HEIGHT      = 0;
+    public static final int FERRISWHEEL_MAXCAP      = 50;
+    public static final int FERRISWHEEL_PRICE       = 70000;
+    public static final int FERRISWHEEL_MOODVALUE   = 30;
+    public static final int FERRISWHEEL_WIDTH       = 3;
+    public static final int FERRISWHEEL_HEIGHT      = 3;
 
-    public static final int ROLLERCOASTER_MAXCAP    = 0;
-    public static final int ROLLERCOASTER_PRICE     = 0;
-    public static final int ROLLERCOASTER_MOODVALUE = 0;
-    public static final int ROLLERCOASTER_WIDTH     = 0;
-    public static final int ROLLERCOASTER_HEIGHT    = 0;
+    public static final int ROLLERCOASTER_MAXCAP    = 10;
+    public static final int ROLLERCOASTER_PRICE     = 200000;
+    public static final int ROLLERCOASTER_MOODVALUE = 50;
+    public static final int ROLLERCOASTER_WIDTH     = 5;
+    public static final int ROLLERCOASTER_HEIGHT    = 4;
 
-    public static final int WATERSLIDE_MAXCAP       = 0;
-    public static final int WATERSLIDE_PRICE        = 0;
-    public static final int WATERSLIDE_MOODVALUE    = 0;
-    public static final int WATERSLIDE_WIDTH        = 0;
-    public static final int WATERSLIDE_HEIGHT       = 0;
+    public static final int WATERSLIDE_MAXCAP       = 15;
+    public static final int WATERSLIDE_PRICE        = 145000;
+    public static final int WATERSLIDE_MOODVALUE    = 25;
+    public static final int WATERSLIDE_WIDTH        = 5;
+    public static final int WATERSLIDE_HEIGHT       = 2;
 }
