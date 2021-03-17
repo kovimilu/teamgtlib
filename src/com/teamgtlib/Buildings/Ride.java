67edcaa510b583063df40cfe4c47562b54d4f7ca
bc2ma1 <bc2ma1@inf.elte.hu>
2021-03-17 15:01:14 +0100

    Update Ride and Visitor

--- src/com/teamgtlib/Buildings/Ride.java
+++ src/com/teamgtlib/Buildings/Ride.java
@@ -1,6 +1,7 @@
 package com.teamgtlib.Buildings;
 
 import com.teamgtlib.NPCs.Visitor;
+
 import java.util.ArrayList;
 
 public class Ride extends Building {
@@ -9,64 +10,76 @@ public class Ride extends Building {
     private int MAXCAP;
     private ArrayList<Visitor> currentPassengers;
     private final RideType type;
-    private ArrayList<Visitor> queue;
+    private ArrayList<Visitor> queue; // bármennyien lehetnek benne, de csak x másodpercenként (next) 1 ember ülhet fel
 
     public Ride(int x, int y, RideType type) {
         super(x, y);
-        this.type   = type;
+        this.type = type;
 
         switch (type) {
             case CAROUSEL -> {
-                MAXCAP      = RideConstants.CAROUSEL_MAXCAP;
-                price       = RideConstants.CAROUSEL_PRICE;
-                moodValue   = RideConstants.CAROUSEL_MOODVALUE;
-                width       = RideConstants.CAROUSEL_WIDTH;
-                height      = RideConstants.CAROUSEL_HEIGHT;
+                MAXCAP = RideConstants.CAROUSEL_MAXCAP;
+                price = RideConstants.CAROUSEL_PRICE;
+                moodValue = RideConstants.CAROUSEL_MOODVALUE;
+                width = RideConstants.CAROUSEL_WIDTH;
+                height = RideConstants.CAROUSEL_HEIGHT;
             }
             case DODGEM -> {
-                MAXCAP      = RideConstants.DODGEM_MAXCAP;
-                price       = RideConstants.DODGEM_PRICE;
-                moodValue   = RideConstants.DODGEM_MOODVALUE;
-                width       = RideConstants.DODGEM_WIDTH;
-                height      = RideConstants.DODGEM_HEIGHT;
+                MAXCAP = RideConstants.DODGEM_MAXCAP;
+                price = RideConstants.DODGEM_PRICE;
+                moodValue = RideConstants.DODGEM_MOODVALUE;
+                width = RideConstants.DODGEM_WIDTH;
+                height = RideConstants.DODGEM_HEIGHT;
             }
             case FERRISWHEEL -> {
-                MAXCAP      = RideConstants.FERRISWHEEL_MAXCAP;
-                price       = RideConstants.FERRISWHEEL_PRICE;
-                moodValue   = RideConstants.FERRISWHEEL_MOODVALUE;
-                width       = RideConstants.FERRISWHEEL_WIDTH;
-                height      = RideConstants.FERRISWHEEL_HEIGHT;
+                MAXCAP = RideConstants.FERRISWHEEL_MAXCAP;
+                price = RideConstants.FERRISWHEEL_PRICE;
+                moodValue = RideConstants.FERRISWHEEL_MOODVALUE;
+                width = RideConstants.FERRISWHEEL_WIDTH;
+                height = RideConstants.FERRISWHEEL_HEIGHT;
             }
             case ROLLERCOASTER -> {
-                MAXCAP      = RideConstants.ROLLERCOASTER_MAXCAP;
-                price       = RideConstants.ROLLERCOASTER_PRICE;
-                moodValue   = RideConstants.ROLLERCOASTER_MOODVALUE;
-                width       = RideConstants.ROLLERCOASTER_WIDTH;
-                height      = RideConstants.ROLLERCOASTER_HEIGHT;
+                MAXCAP = RideConstants.ROLLERCOASTER_MAXCAP;
+                price = RideConstants.ROLLERCOASTER_PRICE;
+                moodValue = RideConstants.ROLLERCOASTER_MOODVALUE;
+                width = RideConstants.ROLLERCOASTER_WIDTH;
+                height = RideConstants.ROLLERCOASTER_HEIGHT;
             }
             case WATERSLIDE -> {
-                MAXCAP      = RideConstants.WATERSLIDE_MAXCAP;
-                price       = RideConstants.WATERSLIDE_PRICE;
-                moodValue   = RideConstants.WATERSLIDE_MOODVALUE;
-                width       = RideConstants.WATERSLIDE_WIDTH;
-                height      = RideConstants.WATERSLIDE_HEIGHT;
+                MAXCAP = RideConstants.WATERSLIDE_MAXCAP;
+                price = RideConstants.WATERSLIDE_PRICE;
+                moodValue = RideConstants.WATERSLIDE_MOODVALUE;
+                width = RideConstants.WATERSLIDE_WIDTH;
+                height = RideConstants.WATERSLIDE_HEIGHT;
             }
         }
     }
 
     public void next() {
+        // queue-ból szed ki legfeljebb MAXCAP-nyi Visitor-t, majd meghívja a startRide()-ot
+    }
+
+    private void startRide() {
+        /* TODO
+        ha timer letelt, vagy teli a Ride kapacitás,
+         akkor start timer
+         ha elég az ember, ezt hívja meg a next
+         */
 
     }
 
-    public void startRide() {
-
+    private void stopRide() {
+        /* TODO
+        cap-ból kiveszi az embereket
+        számértékek hozzáadása
+         */
+        for (Visitor passenger : currentPassengers) {
+            passenger.updateMood(0);
+        }
+        durability -= 0;
     }
 
-    public void stopRide() {
-
-    }
-
-    public void timer() {
+    private void timer() {
 
     }
 
--- src/com/teamgtlib/NPCs/Visitor.java
+++ src/com/teamgtlib/NPCs/Visitor.java
@@ -1,5 +1,9 @@
 package com.teamgtlib.NPCs;
 
 public class Visitor extends NPC {
+    private int mood;
 
+    public void updateMood(int changeBy) {
+        mood += changeBy;
+    }
 }
