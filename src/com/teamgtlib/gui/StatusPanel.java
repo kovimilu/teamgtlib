2d4882fbceba65b8691c8542fd4f7b93c82ab203
bc2ma1 <bc2ma1@inf.elte.hu>
2021-03-23 19:44:59 +0100

    Vertically center labels, remove unnecessary String.getValueOf

--- src/com/teamgtlib/gui/StatusPanel.java
+++ src/com/teamgtlib/gui/StatusPanel.java
@@ -10,22 +10,22 @@ public class StatusPanel extends JPanel{
     JLabel clickLabel = new JLabel( "",SwingConstants.LEFT);
     JLabel exceptionLabel = new JLabel( "",SwingConstants.RIGHT);
     JLabel countLabel = new JLabel( "guests: ..." + "visitors: ....",SwingConstants.CENTER);
-    JLabel budgetLabel = new JLabel( "$"+ String.valueOf(GameFrame.bg.park.player.getBudget()), SwingConstants.RIGHT);
+    JLabel budgetLabel = new JLabel( "$"+ GameFrame.bg.park.player.getBudget(), SwingConstants.RIGHT);
 
     public StatusPanel(){
         setPreferredSize(new Dimension(1280,20));
 
         clickLabel.setForeground(Color.green);
-        clickLabel.setPreferredSize(new Dimension(300,15));
+        clickLabel.setPreferredSize(new Dimension(300,10));
 
         budgetLabel.setForeground(Color.green);
-        budgetLabel.setPreferredSize(new Dimension(300,15));
+        budgetLabel.setPreferredSize(new Dimension(300,10));
 
         exceptionLabel.setForeground(Color.red);
-        exceptionLabel.setPreferredSize(new Dimension(300,15));
+        exceptionLabel.setPreferredSize(new Dimension(300,10));
 
         countLabel.setForeground(Color.green);
-        countLabel.setPreferredSize(new Dimension(300,15));
+        countLabel.setPreferredSize(new Dimension(300,10));
 
         this.add(clickLabel);
         this.add(exceptionLabel);
