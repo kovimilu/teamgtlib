04c068fe34e9427960b43d9548a757281c60fbad
Kovács <dgmvhc@inf.elte.hu>
2021-04-25 17:42:02 +0200

    Visitor AI Demo

--- src/com/teamgtlib/NPCs/Visitor.java
+++ src/com/teamgtlib/NPCs/Visitor.java
@@ -29,8 +29,51 @@ public class Visitor extends NPC {
 
     @Override
     public void whatToDo() {
+        Random rand = new Random();
+        ArrayList<String> random;
+        random = new ArrayList<>();
 
-        if(this.mood < 20 && !currentlyMoving) {
+        String LEAVE = "LEAVE";
+        String EAT = "EAT";
+        String RIDE = "RIDE";
+        String IDLE = "IDLE";
+
+        random.add(IDLE);
+        random.add(LEAVE);
+        random.add(EAT);
+        random.add(RIDE);
+        random.add(random.get(rand.nextInt(4)));
+        // 5
+
+        // 0 - 4
+        if(mood < 40) {
+            random.add(LEAVE);
+            random.add(LEAVE);
+
+            if(mood < 20) {
+                random.add(LEAVE);
+                random.add(LEAVE);
+            }
+            else {
+                random.add(EAT);
+                random.add(RIDE);
+            }
+        }
+        else {
+            random.add(EAT);
+            random.add(EAT);
+            random.add(RIDE);
+            random.add(RIDE);
+        }
+        random.add(RIDE);
+
+
+        String Choice = random.get(rand.nextInt(10));
+        System.out.printf(Choice + "\n");
+
+
+        //LEAVE PARK
+        if(Choice.equals("LEAVE") && !currentlyMoving) {
             //this.currentlyMoving = true;
             this.path = pathfinding(this.x,this.y,10,14);
             //timer();
@@ -40,7 +83,14 @@ public class Visitor extends NPC {
             setVisitorCountAndRefreshLabelText();
             //GameFrame.bg.repaint();
         }
-        if(this.mood >= 50 && !currentlyMoving) {
+        if(Choice.equals("IDLE") && !currentlyMoving) {
+            //this.currentlyMoving = true;
+            this.path = pathfinding(this.x,this.y,this.x,this.y);
+            currentlyMoving = true;
+            wait(1000);
+            move(path);
+        }
+        if(Choice.equals("RIDE")&& !currentlyMoving) {
             //this.currentlyMoving = true;
             this.path = pathfinding(this.x,this.y,10,7);
             this.mood -= 5;
@@ -50,9 +100,9 @@ public class Visitor extends NPC {
             currentlyMoving = true;
             move(path);
         }
-        if(this.mood >= 45 && !currentlyMoving) {
+        if(Choice.equals("EAT") && !currentlyMoving) {
+            //TODO random shop + shop metódusok hívása
             this.path = pathfinding(this.x,this.y,8,9);
-            this.mood -= 50;
             currentlyMoving = true;
             move(path);
             wait(100);
@@ -60,7 +110,6 @@ public class Visitor extends NPC {
         }
 
         //random cuccuk pathfindig rando mridehoz vagy bolthoz vagy haza megy
-        else System.out.printf("qwe");
     }
 
     public void updateMood(int changeBy) {
